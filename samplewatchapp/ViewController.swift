9c18b4a4689a35987ab2d721b70a330688b7de45
8ma3 <syaorann200614567@gmail.com>
2020-10-10 22:40:23 +0900

    Initial Commit

---
+++ samplewatchapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  samplewatchapp
+//
+//  Created by 4423 on 2020/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
